946d78e75da0738d4d469948c98f730dce9595e8
Amit K S <aks@sumologic.com>
2020-04-26 19:45:52 +0530

    PYT: Integrated PostGres DB

--- MiniYT/settings.py
+++ MiniYT/settings.py
@@ -73,11 +73,31 @@ WSGI_APPLICATION = 'MiniYT.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+    "default": {
+
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+
+        "NAME": "PYT",
+
+        "USER": "aks",
+
+        "PASSWORD": "",
+
+        "HOST": "localhost",
+
+        "PORT": "5432",
+
     }
+
 }
 
 
