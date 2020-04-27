1a34de2488cf2635587638230ee8bcf01522c121
Amit K S <aks@sumologic.com>
2020-04-27 16:43:12 +0530

    PYT: Merged migrations for docker compatibilty

--- MiniYT/wsgi.py
+++ MiniYT/wsgi.py
@@ -9,16 +9,15 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os, json, threading
 from django.core.wsgi import get_wsgi_application
-from data.models import APIFetch
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MiniYT.settings')
 
 application = get_wsgi_application()
 
-# clear APIFetch model data
-APIFetch.objects.all().delete()
+# # clear APIFetch model data
+# APIFetch.objects.all().delete()
 
-# handler to start fetching API - set to false
-APIFetchEntry = APIFetch(fetchAPI = False)
-APIFetchEntry.save()
+# # handler to start fetching API - set to false
+# APIFetchEntry = APIFetch(fetchAPI = False)
+# APIFetchEntry.save()
 
